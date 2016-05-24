157dfb692c3b56313ae555b2b76e9b62470e93c2
Anand B Muley <Anand.Muley@ad.infosys.com>
2016-05-24 17:07:20 +0530

    Working on creating grocery list

--- src/main/webapp/js/controllers/ListController.js
+++ src/main/webapp/js/controllers/ListController.js
@@ -1,17 +1,36 @@
 controllers.controller('ListController',['$scope','ProductResource',function($scope,Product){
 	
 	$scope.products = Product.query();
-	$scope.list = [{
-		product : '',
-		quantity : 0
-	}];
+	$scope.list = [];
+	
 	
 	$scope.enlist = function(product){
 		$scope.list.push(product);
 	}
 	
+	$scope.deleteRow = function(item){
+		var index = $scope.list.indexOf(item);
+		if(index!=-1){
+			$scope.list.splice(index,1);
+		}
+	}
+	
 	$scope.addProduct = function(){
-		
+		$scope.list.push({
+			product : null,
+			quantity : 0
+		});	
+	}
+	
+	$scope.addProduct();
+	
+	$scope.getMeasuredIn = function(product){
+		var measuredIn = '';
+		if(product!=null){
+			var prod = JSON.parse(product);
+			measuredIn = prod.measuredIn;	
+		}
+		return measuredIn;
 	}
 	
 }]);
