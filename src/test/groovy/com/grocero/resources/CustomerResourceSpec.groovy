18491e6f3c5eb812040fa049ad105824c2d7205e
Anand Muley <anand.muley@cognizant.com>
2017-10-13 15:50:50 +0000

    [AM] #4 Improved unit test case

--- src/test/groovy/com/grocero/resources/CustomerResourceSpec.groovy
+++ src/test/groovy/com/grocero/resources/CustomerResourceSpec.groovy
@@ -5,6 +5,7 @@ import com.grocero.builders.MasterListDtoBuilder
 import com.grocero.dtos.CustomerDto
 import com.grocero.dtos.MasterListDto
 import com.grocero.dtos.ResponseDto
+import com.grocero.exceptions.CustomerServiceException
 import com.grocero.exceptions.NoDataFoundException
 import com.grocero.services.CustomerService
 import com.grocero.shared.SharedSpecification
@@ -27,11 +28,77 @@ class CustomerResourceSpec extends SharedSpecification {
         customerResource = new CustomerResource(mockCustomerService)
     }
 
+    def "getAll - should return error if no customers found"() {
+        given: "customers are not registered"
+        CustomerDto expected = new CustomerDtoBuilder().build()
+        NoDataFoundException ex = new NoDataFoundException("No customers found")
+        1 * mockCustomerService.getAll() >> {throw ex}
+
+        when: "getAll is called"
+        Response response = customerResource.getAll()
+
+
+        then: "customer records are sent in response"
+        response.status == 404
+        response.hasEntity() == false
+    }
+
+    def "getAll - should get all customers"() {
+        given: "customers are registered"
+        CustomerDto expected = new CustomerDtoBuilder().build()
+        1 * mockCustomerService.getAll() >> [expected]
+
+        when: "getAll is called"
+        Response response = customerResource.getAll()
+
+
+        then: "customer records are sent in response"
+        response.status == 200
+        Set<CustomerDto> customers = response.entity
+        customers.size() == 1
+        CustomerDto actual = customers[0]
+        actual.authToken == expected.authToken
+        actual.password == expected.password
+        actual.name == expected.name
+        actual.id == expected.id
+        actual.role == expected.role
+        actual.username == expected.username
+    }
+
+    def "create - should create a customer"() {
+        given: "details of the customer to be added"
+        CustomerDto expected = new CustomerDtoBuilder().build()
+
+        1 * mockCustomerService.createOrUpdate({ CustomerDto custDto ->
+            custDto.username == expected.username
+            custDto.role == expected.role
+            custDto.id == expected.id
+            custDto.name == expected.name
+            custDto.password == expected.password
+            custDto.authToken == expected.authToken
+            true
+        })
+
+        when: "create method is invoked"
+        Response response = customerResource.create(expected)
+
+        then: "success response is received"
+        response.status == 200
+        CustomerDto actual = response.entity
+        actual.username == expected.username
+        actual.role == expected.role
+        actual.id == expected.id
+        actual.name == expected.name
+        actual.password == expected.password
+        actual.authToken == expected.authToken
+
+    }
+
     def "authenticate - should return the auth token if authentication is successful"() {
         given: "username and password"
         def authToken = UUID.randomUUID().toString()
         CustomerDto customerDto = new CustomerDtoBuilder().username(username).password(password).build()
-        1 * mockCustomerService.findByUsernameAndPassword(username,password) >> Optional.of(new CustomerDtoBuilder().authToken(authToken).build())
+        1 * mockCustomerService.findByUsernameAndPassword(username, password) >> Optional.of(new CustomerDtoBuilder().authToken(authToken).build())
 
         when: "authenticate method is called"
         Response actualResponse = customerResource.authenticate(customerDto)
@@ -56,6 +123,33 @@ class CustomerResourceSpec extends SharedSpecification {
         actualResponse.status == 404
         actual.message == "Record not found"
 
+    }
+
+    def "createMasterList - should return an error if the customer not found"() {
+        given: "customerId and masterlist to save"
+        def customerId = "CID202"
+        def productName = "Apple"
+        def listName = "Master List"
+        MasterListDto expected = new MasterListDtoBuilder().id(randomId)
+                .name(listName).items([productName] as LinkedHashSet).build()
+        CustomerServiceException ex = new CustomerServiceException()
+
+        1 * mockCustomerService.createOrUpdate({ MasterListDto dto ->
+            assert dto.id == randomId
+            assert dto.customerId == customerId
+            assert dto.name == listName
+            assert dto.items.size() == 1
+            assert dto.items[0] == productName
+            true
+        }) >> { throw ex }
+
+        when: "createMasterList is invoked"
+        Response response = customerResource.createMasterList(customerId, expected)
+
+        then: "all details are sent in response"
+        response.status == 404
+        ResponseDto actual = response.entity
+        actual.message == ex.message
     }
 
     def "createMasterList - should create a masterlist"() {
